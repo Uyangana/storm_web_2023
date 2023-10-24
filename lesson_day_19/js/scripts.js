3acbb7e8f3a62758095de112c8e62e9a2319d3a1
Uyangana <uyangaa0104@gmail.com>
2023-10-24 10:37:00 +0800

    JS

---
+++ lesson_day_19/js/scripts.js
@@ -0,0 +1,37 @@
+console.log("Lesson Day 19 - JS Control structures exercises");
+
+// variables
+//number variables
+let too =10;
+let too2 =20;
+let too3 =30;
+
+//string variables
+let ner1 = 'Bold';
+let ner2 = 'Tulga';
+let ner3 = 'Naraa';
+
+//boolean variable
+let unen = true;
+let hudal = false;
+
+let isMongolian = true;
+lethasMongolianPassword=false;
+
+if (isMongolian) {
+    console.log('You are mongolian.');
+} else if (isMongolian && hasMongolianPassword) {
+    console.log('You are mongolian.');
+} else {
+    console.log('You are not mongolian.');
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+let i = 0;
+while (i < 10) {
+    console.log(i);
+    i++
+}
