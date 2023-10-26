56a7cb9449dd6d8db2aec8dd6d2e42c63e4c24dc
Uyangana <uyangaa0104@gmail.com>
2023-10-26 13:04:23 +0800

    dom

---
+++ lesson_day_20/js/arrays_script.js
@@ -0,0 +1,19 @@
+console.log('Lesson Day 20 - JS Arrays');
+
+var myNumbers = [5,8,103,32,7]; //array of numbers
+console.log(myNumbers); // [5,8,103,32,7]
+
+var shoppingList = ['milk','eggs','bread','cheese','butter']; //array of string
+console.log(shoppinList); 
+
+var randomList = [5,7 'milk', 'eggs', true, false];
+console.log(randomList);
+
+//accessing array elements
+
+console.log('before change shoppingList: ', shoppingList);
+
+//change array elements
+console.log('before change shoppingList: ', shoppingList);
+shoppingList[3] = 'meat';
+console.log('after change shoppingList; ', shoppingList);
